0b996bc34b8490db4345cd0cb4de2a623edeb171
Mubashir P A <mubashirpa2002@gmail.com>
2023-02-25 14:42:51 +0530

    Added viewmodels

--- app/src/main/java/edumate/app/presentation/MainActivity.kt
+++ app/src/main/java/edumate/app/presentation/MainActivity.kt
@@ -3,16 +3,20 @@ package edumate.app.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import edumate.app.presentation.ui.theme.EdumateTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             EdumateTheme {
-                EdumateApp()
+                EdumateApp(viewModel.isLoggedIn)
             }
         }
     }
