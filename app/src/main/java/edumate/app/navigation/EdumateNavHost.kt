468fc9250e454f5a646678210080ae168553d89f
Mubashir P A <mubashirpa2002@gmail.com>
2023-02-25 14:41:43 +0530

    Added screens

--- app/src/main/java/edumate/app/navigation/EdumateNavHost.kt
+++ app/src/main/java/edumate/app/navigation/EdumateNavHost.kt
@@ -2,10 +2,26 @@ package edumate.app.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import edumate.app.presentation.home.screen.HomeScreen
 
 @Composable
 fun EdumateNavHost(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = Routes.Graph.AUTHENTICATION
 ) {
-
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+        authentication(navController)
+        composable(route = Routes.Screen.HOME_SCREEN) {
+            HomeScreen()
+        }
+    }
 }
