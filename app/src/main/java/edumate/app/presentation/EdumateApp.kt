a8ae849e266f74db6ad9f147e21f010bbea6a664
Mubashir P A <mubashirpa2002@gmail.com>
2023-02-25 14:41:26 +0530

    Added navigate to login page if not logged in

--- app/src/main/java/edumate/app/presentation/EdumateApp.kt
+++ app/src/main/java/edumate/app/presentation/EdumateApp.kt
@@ -6,13 +6,16 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import edumate.app.navigation.EdumateNavHost
+import edumate.app.navigation.Routes
 
 @Composable
-fun EdumateApp() {
+fun EdumateApp(
+    isLoggedIn: Boolean
+) {
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-        EdumateNavHost()
+        if (isLoggedIn) EdumateNavHost(startDestination = Routes.Screen.HOME_SCREEN) else EdumateNavHost()
     }
 }
