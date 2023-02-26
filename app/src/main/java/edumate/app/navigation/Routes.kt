e46a2cd193295958aba7510a8d4f14f50b879428
Mubashir P A <mubashirpa2002@gmail.com>
2023-02-26 10:08:46 +0530

    Added arguments

--- app/src/main/java/edumate/app/navigation/Routes.kt
+++ app/src/main/java/edumate/app/navigation/Routes.kt
@@ -6,9 +6,16 @@ object Routes {
         const val GET_STARTED_SCREEN = "get_started_screen"
         const val LOGIN_SCREEN = "login_screen"
         const val REGISTER_SCREEN = "register_screen"
+        const val RECOVER_SCREEN = "recover_screen"
     }
 
     object Graph {
         const val AUTHENTICATION = "authentication"
+    }
+
+    object Args {
+        // RecoverScreen
+        const val RECOVER_EMAIL = "email"
+        const val RECOVER_SCREEN = "?$RECOVER_EMAIL={$RECOVER_EMAIL}"
     }
 }
