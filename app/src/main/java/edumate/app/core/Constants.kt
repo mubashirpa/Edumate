91beaf9fa702c913069e769903040565f0b065ba
Mubashir P A <mubashirpa2002@gmail.com>
2023-02-26 10:08:33 +0530

    Added arguments

--- app/src/main/java/edumate/app/core/Constants.kt
+++ app/src/main/java/edumate/app/core/Constants.kt
@@ -1,5 +1,7 @@
 package edumate.app.core
 
 object Constants {
-    const val GET_STARTED_SCREEN_BACKDROP = "file:///android_asset/images/get_started.png"
+    const val GET_STARTED_BACKDROP_ASSET_URL = "file:///android_asset/images/get_started.png"
+    const val GET_STARTED_BACKDROP_URL =
+        "https://firebasestorage.googleapis.com/v0/b/edu-mate-app.appspot.com/o/get_started.jpg?alt=media&token=6b9e6215-c0a4-4046-a15d-42cb5a102986"
 }
