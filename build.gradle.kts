762c40842e488d9dae484547ab4c3ef044403904
mubashirpa <mubashirpa2002@gmail.com>
2023-05-29 18:48:43 +0530

    Added firebase crashlytics plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.devtools.ksp) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.gradle.ktlint) apply false
     alias(libs.plugins.gradle.versions)
