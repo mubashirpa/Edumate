522b1b4c58c4767c66cc6aab8aa4bfc9707acc23
Mubashir P A <mubashirpa2002@gmail.com>
2023-12-20 17:17:22 +0530

    Update OneSignal initialisation

--- app/src/main/java/edumate/app/EdumateApplication.kt
+++ app/src/main/java/edumate/app/EdumateApplication.kt
@@ -2,8 +2,11 @@ package edumate.app
 
 import android.app.Application
 import com.onesignal.OneSignal
+import com.onesignal.debug.LogLevel
 import dagger.hilt.android.HiltAndroidApp
-import edumate.app.core.Constants
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @HiltAndroidApp
 class EdumateApplication : Application() {
@@ -11,8 +14,14 @@ class EdumateApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        // Verbose Logging set to help debug issues, TODO("Remove before releasing app").
+        OneSignal.Debug.logLevel = LogLevel.VERBOSE
         // OneSignal Initialization
-        OneSignal.initWithContext(this)
-        OneSignal.setAppId(Constants.ONESIGNAL_APP_ID)
+        OneSignal.initWithContext(this, BuildConfig.ONESIGNAL_APP_ID)
+        // RequestPermission will show the native Android notification permission prompt.
+        // NOTE: It's recommended to use a OneSignal In-App Message to prompt instead.
+        CoroutineScope(Dispatchers.IO).launch {
+            OneSignal.Notifications.requestPermission(true)
+        }
     }
 }
