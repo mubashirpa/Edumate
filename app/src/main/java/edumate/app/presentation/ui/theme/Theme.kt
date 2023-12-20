61592c10a9584b8558208d01dd25a4dba22f55ee
Mubashir P A <mubashirpa2002@gmail.com>
2023-12-20 17:18:11 +0530

    Removed manual edge to edge configurations

--- app/src/main/java/edumate/app/presentation/ui/theme/Theme.kt
+++ app/src/main/java/edumate/app/presentation/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package edumate.app.presentation.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,11 +8,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
     primary = md_theme_dark_primary,
@@ -94,21 +89,6 @@ fun EdumateTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
-    }
-
-    val view = LocalView.current
-    val window = (view.context as Activity).window
-
-    // Turn off the decor fitting system windows, which means we need to through handling insets
-    WindowCompat.setDecorFitsSystemWindows(window, false)
-
-    if (!view.isInEditMode) {
-        val systemUiController = rememberSystemUiController()
-        val useDarkIcons = !darkTheme
-        SideEffect {
-            systemUiController.statusBarDarkContentEnabled = useDarkIcons
-            systemUiController.navigationBarDarkContentEnabled = useDarkIcons
-        }
     }
 
     MaterialTheme(
