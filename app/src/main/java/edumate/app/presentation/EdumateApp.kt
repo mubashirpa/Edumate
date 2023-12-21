cfe44cd41e0f23a84dca9b7bddc324463fb29f7f
Mubashir P A <mubashirpa2002@gmail.com>
2023-12-21 23:12:51 +0530

    Optimised code

--- app/src/main/java/edumate/app/presentation/EdumateApp.kt
+++ app/src/main/java/edumate/app/presentation/EdumateApp.kt
@@ -1,126 +1,66 @@
 package edumate.app.presentation
 
-import android.annotation.SuppressLint
-import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import edumate.app.navigation.EdumateNavHost
 import edumate.app.navigation.Routes
-import edumate.app.navigation.Screen
-import edumate.app.presentation.components.EdumateDrawerContent
 import edumate.app.presentation.components.EdumateSnackbarHost
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun EdumateApp(isLoggedIn: Boolean) {
+    val navController = rememberNavController()
     val snackbarHostState = remember { SnackbarHostState() }
-    val snackbarScope = rememberCoroutineScope()
+    val startDestination =
+        if (isLoggedIn) Routes.Screen.HOME_SCREEN else Routes.Graph.AUTHENTICATION
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         snackbarHost = {
-            EdumateSnackbarHost(
-                hostState = snackbarHostState,
-                modifier = Modifier.navigationBarsPadding(),
-            )
+            Box(modifier = Modifier.fillMaxWidth()) {
+                EdumateSnackbarHost(hostState = snackbarHostState)
+            }
         },
-        contentWindowInsets = WindowInsets(0, 0, 0, 0),
+        contentWindowInsets = WindowInsets(0),
     ) { innerPadding ->
-        MPSNavigationWrapper(
-            snackbarHostState = snackbarHostState,
-            snackbarScope = snackbarScope,
+        EdumateContent(
+            navController = navController,
             modifier =
                 Modifier
                     .padding(innerPadding)
                     .consumeWindowInsets(innerPadding)
-                    .windowInsetsPadding(
-                        WindowInsets.safeDrawing.only(WindowInsetsSides.Horizontal),
-                    ),
-            isLoggedIn = isLoggedIn,
+                    .windowInsetsPadding(WindowInsets.safeDrawing.only(WindowInsetsSides.Horizontal)),
+            startDestination = startDestination,
+            snackbarHostState = snackbarHostState,
         )
     }
 }
 
 @Composable
-private fun MPSNavigationWrapper(
-    snackbarHostState: SnackbarHostState,
-    snackbarScope: CoroutineScope,
-    modifier: Modifier = Modifier,
-    isLoggedIn: Boolean,
-) {
-    val coroutineScope = rememberCoroutineScope()
-    val drawerState = rememberDrawerState(DrawerValue.Closed)
-    val rootNavController = rememberNavController()
-    val startDestination =
-        if (isLoggedIn) Routes.Screen.HOME_SCREEN else Routes.Graph.AUTHENTICATION
-    val navBackStackEntry by rootNavController.currentBackStackEntryAsState()
-    val currentDestination = navBackStackEntry?.destination
-    val gesturesEnabled = currentDestination?.route == Screen.HomeScreen.route
-
-    BackHandler(drawerState.isOpen) {
-        coroutineScope.launch {
-            drawerState.close()
-        }
-    }
-
-    ModalNavigationDrawer(
-        drawerContent = {
-            EdumateDrawerContent(
-                onItemClick = { index ->
-                    coroutineScope.launch {
-                        drawerState.close()
-                    }
-                    when (index) {
-                        0 -> {
-                            rootNavController.navigate(Screen.ProfileScreen.route)
-                        }
-
-                        1 -> {
-                            rootNavController.navigate(Screen.SettingsScreen.route)
-                        }
-                    }
-                },
-            )
-        },
-        drawerState = drawerState,
-        gesturesEnabled = gesturesEnabled,
-    ) {
-        Box(modifier = modifier) {
-            EdumateAppContent(
-                navController = rootNavController,
-                startDestination = startDestination,
-                drawerState = drawerState,
-                snackbarHostState = snackbarHostState,
-                snackbarScope = snackbarScope,
-            )
-        }
-    }
-}
-
-@Composable
-private fun EdumateAppContent(
+private fun EdumateContent(
     navController: NavHostController,
+    modifier: Modifier = Modifier,
     startDestination: String = Routes.Graph.AUTHENTICATION,
-    drawerState: DrawerState,
     snackbarHostState: SnackbarHostState,
-    snackbarScope: CoroutineScope,
 ) {
     EdumateNavHost(
         navController = navController,
-        modifier = Modifier.fillMaxSize(),
+        modifier = modifier,
         startDestination = startDestination,
-        drawerState = drawerState,
         snackbarHostState = snackbarHostState,
-        snackbarScope = snackbarScope,
     )
 }
