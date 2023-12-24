30c84ae81fdc8b97be2d4bfe06c6b51219df7c2f
Mubashir P A <mubashirpa2002@gmail.com>
2023-12-24 10:11:06 +0530

    Optimised code and improved performance

--- app/src/main/java/edumate/app/core/Resource.kt
+++ app/src/main/java/edumate/app/core/Resource.kt
@@ -1,9 +1,11 @@
 package edumate.app.core
 
 sealed class Resource<T>(val data: T? = null, val message: UiText? = null) {
-    class Success<T>(data: T) : Resource<T>(data)
-
     class Error<T>(message: UiText, data: T? = null) : Resource<T>(data, message)
 
     class Loading<T>(data: T? = null) : Resource<T>(data)
+
+    class Success<T>(data: T?) : Resource<T>(data)
+
+    class Unknown<T> : Resource<T>()
 }
