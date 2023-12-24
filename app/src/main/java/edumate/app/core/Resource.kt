aca0d1ae4e12f137c69a866d0fb5b44e863ea8ce
Mubashir P A <mubashirpa2002@gmail.com>
2023-12-24 10:35:35 +0530

    Deprecated code and will be removed later

--- app/src/main/java/edumate/app/core/Resource.kt
+++ app/src/main/java/edumate/app/core/Resource.kt
@@ -6,6 +6,4 @@ sealed class Resource<T>(val data: T? = null, val message: UiText? = null) {
     class Loading<T>(data: T? = null) : Resource<T>(data)
 
     class Success<T>(data: T?) : Resource<T>(data)
-
-    class Unknown<T> : Resource<T>()
 }
