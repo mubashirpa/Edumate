f25fdc1c6b933537844876d00e9d38d3d5be5f6d
Mubashir P A <mubashirpa2002@gmail.com>
2025-01-19 17:17:50 +0530

    feat: Update Google Web Client ID.

--- app/src/main/java/app/edumate/core/Constants.kt
+++ app/src/main/java/app/edumate/core/Constants.kt
@@ -7,7 +7,7 @@ object Constants {
         "https://firebasestorage.googleapis.com/v0/b/edu-mate-app.appspot.com/o/get_started.jpg?alt=media&token=6b9e6215-c0a4-4046-a15d-42cb5a102986"
     const val BACKDROP_GET_STARTED_LOCAL = "file:///android_asset/images/get_started.png"
     const val WEB_GOOGLE_CLIENT_ID =
-        "397578092741-alqtcebud1r0tsddkm90gj3bfjebkdk0.apps.googleusercontent.com"
+        "242656945367-ipk4n5tmvqgt9p8kqb9bk0dckgc8lefn.apps.googleusercontent.com"
 }
 
 object PreferencesKeys {
