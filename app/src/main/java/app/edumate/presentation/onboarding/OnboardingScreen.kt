0fadbcbe1d2d96064ce99fa2addeb3067a7a1a5c
Mubashir P A <mubashirpa2002@gmail.com>
2025-01-19 11:28:59 +0530

    feat: Add onboarding screen

---
+++ app/src/main/java/app/edumate/presentation/onboarding/OnboardingScreen.kt
@@ -0,0 +1,115 @@
+package app.edumate.presentation.onboarding
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import app.edumate.R
+import app.edumate.core.Constants
+import app.edumate.presentation.theme.EdumateTheme
+import coil3.compose.AsyncImage
+import coil3.compose.rememberAsyncImagePainter
+import coil3.request.ImageRequest
+import coil3.request.crossfade
+
+@Composable
+fun OnboardingScreen(
+    onNavigateToSignIn: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val backgroundPlaceholder = rememberAsyncImagePainter(Constants.BACKDROP_GET_STARTED_LOCAL)
+    val colorStops =
+        arrayOf(
+            0.1f to Color.Transparent,
+            1f to Color.Black,
+        )
+
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.TopCenter,
+    ) {
+        AsyncImage(
+            model =
+                ImageRequest
+                    .Builder(LocalContext.current)
+                    .data(Constants.BACKDROP_GET_STARTED)
+                    .crossfade(true)
+                    .build(),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            placeholder = backgroundPlaceholder,
+            error = backgroundPlaceholder,
+            contentScale = ContentScale.Crop,
+        )
+        Box(
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .background(Brush.verticalGradient(colorStops = colorStops)),
+        )
+        Column(
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .safeContentPadding()
+                    .padding(horizontal = 24.dp, vertical = 32.dp),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = stringResource(id = R.string.app_name).lowercase(),
+                color = Color.White,
+                style = MaterialTheme.typography.headlineLarge,
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            Text(
+                text = stringResource(id = R.string.get_started_message),
+                color = Color.White,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.bodyLarge,
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Button(
+                onClick = onNavigateToSignIn,
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .height(56.dp),
+                shape = MaterialTheme.shapes.large,
+            ) {
+                Text(
+                    text = stringResource(id = R.string.get_started),
+                    style = MaterialTheme.typography.bodyLarge,
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun OnboardingScreenPreview() {
+    EdumateTheme {
+        OnboardingScreen(onNavigateToSignIn = {})
+    }
+}
