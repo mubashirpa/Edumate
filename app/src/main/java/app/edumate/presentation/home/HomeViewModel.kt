036abea0fd1a8e63259ace3210c75cfe818b69a5
Mubashir P A <mubashirpa2002@gmail.com>
2025-01-28 22:54:35 +0530

    refactor: Use member package for UserRole
    This commit updates the `UserRole` class import to use the new `member` package instead of `users`. This change aligns with the updated project structure.

--- app/src/main/java/app/edumate/presentation/home/HomeViewModel.kt
+++ app/src/main/java/app/edumate/presentation/home/HomeViewModel.kt
@@ -11,7 +11,7 @@ import androidx.navigation.toRoute
 import app.edumate.R
 import app.edumate.core.Result
 import app.edumate.core.UiText
-import app.edumate.domain.model.users.UserRole
+import app.edumate.domain.model.member.UserRole
 import app.edumate.domain.usecase.authentication.GetCurrentUserUseCase
 import app.edumate.domain.usecase.courses.DeleteCourseUseCase
 import app.edumate.domain.usecase.courses.GetCoursesUseCase
