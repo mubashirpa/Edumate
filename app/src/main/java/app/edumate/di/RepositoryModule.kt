6c9e0b10a499946a6170e5e405e8ea5ee52665c8
Mubashir P A <mubashirpa2002@gmail.com>
2025-01-28 13:02:12 +0530

    feat: Add StorageRepository to Koin module

--- app/src/main/java/app/edumate/di/RepositoryModule.kt
+++ app/src/main/java/app/edumate/di/RepositoryModule.kt
@@ -3,9 +3,11 @@ package app.edumate.di
 import app.edumate.data.repository.AuthenticationRepositoryImpl
 import app.edumate.data.repository.CourseRepositoryImpl
 import app.edumate.data.repository.MemberRepositoryImpl
+import app.edumate.data.repository.StorageRepositoryImpl
 import app.edumate.domain.repository.AuthenticationRepository
 import app.edumate.domain.repository.CourseRepository
 import app.edumate.domain.repository.MemberRepository
+import app.edumate.domain.repository.StorageRepository
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
@@ -15,4 +17,5 @@ val repositoryModule =
         singleOf(::AuthenticationRepositoryImpl) { bind<AuthenticationRepository>() }
         singleOf(::CourseRepositoryImpl) { bind<CourseRepository>() }
         singleOf(::MemberRepositoryImpl) { bind<MemberRepository>() }
+        singleOf(::StorageRepositoryImpl) { bind<StorageRepository>() }
     }
