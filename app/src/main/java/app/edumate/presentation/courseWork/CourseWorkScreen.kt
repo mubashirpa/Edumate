8ddbc826f3e232a5806437bc49d7f3007af0cb46
Mubashir P A <mubashirpa2002@gmail.com>
2025-01-29 20:39:24 +0530

    feat: Add snackbar host to CourseWorkScreen
    This commit adds a `SnackbarHost` to the `CourseWorkScreen` to display snackbar messages.

--- app/src/main/java/app/edumate/presentation/courseWork/CourseWorkScreen.kt
+++ app/src/main/java/app/edumate/presentation/courseWork/CourseWorkScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -137,6 +138,9 @@ fun CourseWorkScreen(
                 },
                 scrollBehavior = scrollBehavior,
             )
+        },
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
         },
         floatingActionButton = {
             if (isCurrentUserTeacher) {
