28bf9fec79500a36ca9ec7d37cd7d59b76d3595b
Mubashir P A <mubashirpa2002@gmail.com>
2025-01-29 23:35:05 +0530

    feat: Add CreateCourseWorkViewModel to Koin module

--- app/src/main/java/app/edumate/di/ViewModelModule.kt
+++ app/src/main/java/app/edumate/di/ViewModelModule.kt
@@ -3,6 +3,7 @@ package app.edumate.di
 import app.edumate.presentation.courseDetails.CourseDetailsViewModel
 import app.edumate.presentation.courseWork.CourseWorkViewModel
 import app.edumate.presentation.createCourse.CreateCourseViewModel
+import app.edumate.presentation.createCourseWork.CreateCourseWorkViewModel
 import app.edumate.presentation.home.HomeViewModel
 import app.edumate.presentation.main.MainViewModel
 import app.edumate.presentation.newPassword.NewPasswordViewModel
@@ -19,6 +20,7 @@ val viewModelModule =
         viewModelOf(::CourseDetailsViewModel)
         viewModelOf(::CourseWorkViewModel)
         viewModelOf(::CreateCourseViewModel)
+        viewModelOf(::CreateCourseWorkViewModel)
         viewModelOf(::HomeViewModel)
         viewModelOf(::MainViewModel)
         viewModelOf(::NewPasswordViewModel)
