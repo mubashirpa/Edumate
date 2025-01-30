132b94ea3d1d828830d4ad9bf34481f448fdd02b
Mubashir P A <mubashirpa2002@gmail.com>
2025-01-30 18:41:49 +0530

    feat: Add mapping functions between CourseWork DTO and domain models
    This commit introduces new mapping functions to convert between the `CourseWork` data transfer object (DTO) and its corresponding domain model, and between `MultipleChoiceQuestion` DTO and domain models.

--- app/src/main/java/app/edumate/data/mapper/CourseWorkMapper.kt
+++ app/src/main/java/app/edumate/data/mapper/CourseWorkMapper.kt
@@ -1,7 +1,9 @@
 package app.edumate.data.mapper
 
 import app.edumate.data.remote.dto.courseWork.CourseWorkDto
+import app.edumate.data.remote.dto.courseWork.CourseWorkTypeDto
 import app.edumate.data.remote.dto.courseWork.MultipleChoiceQuestionDto
+import app.edumate.data.remote.dto.courseWork.SubmissionModificationModeDto
 import app.edumate.domain.model.courseWork.CourseWork
 import app.edumate.domain.model.courseWork.CourseWorkType
 import app.edumate.domain.model.courseWork.MultipleChoiceQuestion
@@ -28,3 +30,25 @@ fun CourseWorkDto.toCourseWorkDomainModel(): CourseWork =
     )
 
 fun MultipleChoiceQuestionDto.toMultipleChoiceQuestionDomainModel(): MultipleChoiceQuestion = MultipleChoiceQuestion(choices = choices)
+
+fun CourseWork.toCourseWorkDto(): CourseWorkDto =
+    CourseWorkDto(
+        id = id,
+        courseId = courseId,
+        creatorUserId = creatorUserId,
+        alternateLink = alternateLink,
+        creationTime = creationTime,
+        description = description,
+        dueTime = dueTime,
+        maxPoints = maxPoints,
+        multipleChoiceQuestion = multipleChoiceQuestion?.toMultipleChoiceQuestionDto(),
+        title = title,
+        updateTime = updateTime,
+        submissionModificationMode =
+            submissionModificationMode?.let {
+                enumValueOf<SubmissionModificationModeDto>(it.name)
+            },
+        workType = workType?.let { enumValueOf<CourseWorkTypeDto>(it.name) },
+    )
+
+fun MultipleChoiceQuestion.toMultipleChoiceQuestionDto(): MultipleChoiceQuestionDto = MultipleChoiceQuestionDto(choices = choices)
