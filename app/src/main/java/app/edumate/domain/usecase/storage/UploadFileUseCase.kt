05c2e50f4ba1ad72f2c7d341a92f07d27eef4647
Mubashir P A <mubashirpa2002@gmail.com>
2025-01-31 12:21:22 +0530

    feat: Refactor UploadFileUseCase to use Result and handle errors
    This commit refactors the `UploadFileUseCase` to return a `Flow<Result<FileUploadState>>` instead of `Flow<FileUploadState>`, allowing it to communicate success, loading, and error states. It also adds comprehensive error handling for various exceptions that might occur during the file upload process.

--- app/src/main/java/app/edumate/domain/usecase/storage/UploadFileUseCase.kt
+++ app/src/main/java/app/edumate/domain/usecase/storage/UploadFileUseCase.kt
@@ -1,10 +1,17 @@
 package app.edumate.domain.usecase.storage
 
+import app.edumate.R
+import app.edumate.core.Result
+import app.edumate.core.UiText
 import app.edumate.domain.model.FileUploadState
 import app.edumate.domain.repository.StorageRepository
+import io.github.jan.supabase.exceptions.HttpRequestException
+import io.github.jan.supabase.exceptions.RestException
+import io.ktor.client.plugins.HttpRequestTimeoutException
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import java.io.File
 
@@ -12,9 +19,25 @@ class UploadFileUseCase(
     private val storageRepository: StorageRepository,
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
 ) {
-    suspend operator fun invoke(
-        bucketName: String,
-        filePath: String,
+    operator fun invoke(
+        bucketId: String,
+        path: String,
         file: File,
-    ): Flow<FileUploadState> = storageRepository.uploadFile(bucketName, filePath, file).flowOn(ioDispatcher)
+    ): Flow<Result<FileUploadState>> =
+        flow {
+            try {
+                emit(Result.Loading())
+                storageRepository.uploadFile(bucketId, path, file).collect {
+                    emit(Result.Success(it))
+                }
+            } catch (_: RestException) {
+                emit(Result.Error(UiText.StringResource(R.string.error_unexpected)))
+            } catch (_: HttpRequestTimeoutException) {
+                emit(Result.Error(UiText.StringResource(R.string.error_timeout_exception)))
+            } catch (_: HttpRequestException) {
+                emit(Result.Error(UiText.StringResource(R.string.error_network_exception)))
+            } catch (_: Exception) {
+                emit(Result.Error(UiText.StringResource(R.string.error_unknown)))
+            }
+        }.flowOn(ioDispatcher)
 }
