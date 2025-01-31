c693cfa564e131087dd94b91ff96f4f3675fa6ac
Mubashir P A <mubashirpa2002@gmail.com>
2025-01-31 18:10:04 +0530

    feat: Implement updating course work
    This commit adds the ability to update existing course work.

--- app/src/main/java/app/edumate/data/repository/CourseWorkRepositoryImpl.kt
+++ app/src/main/java/app/edumate/data/repository/CourseWorkRepositoryImpl.kt
@@ -2,9 +2,14 @@ package app.edumate.data.repository
 
 import app.edumate.core.Supabase
 import app.edumate.data.remote.dto.courseWork.CourseWorkDto
+import app.edumate.data.remote.dto.material.MaterialDto
 import app.edumate.domain.repository.CourseWorkRepository
 import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.postgrest.query.Columns
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 
 class CourseWorkRepositoryImpl(
     private val postgrest: Postgrest,
@@ -32,6 +37,36 @@ class CourseWorkRepositoryImpl(
                     eq(Supabase.Column.ID, id)
                 }
             }.decodeSingleOrNull()
+
+    override suspend fun updateCourseWork(
+        id: String,
+        title: String,
+        description: String?,
+        choices: List<String>?,
+        materials: List<MaterialDto>?,
+        maxPoints: Int?,
+        dueTime: String?,
+    ): CourseWorkDto =
+        postgrest[Supabase.Table.COURSE_WORKS]
+            .update(
+                {
+                    set(Supabase.Column.TITLE, title)
+                    set(Supabase.Column.DESCRIPTION, description)
+                    set(Supabase.Column.CHOICES, choices)
+                    set(Supabase.Column.MATERIALS, materials)
+                    set(Supabase.Column.MAX_POINTS, maxPoints)
+                    set(Supabase.Column.DUE_TIME, dueTime)
+
+                    val now: Instant = Clock.System.now()
+                    val updateTime = now.toLocalDateTime(TimeZone.UTC)
+                    set(Supabase.Column.UPDATE_TIME, updateTime)
+                },
+            ) {
+                select()
+                filter {
+                    eq(Supabase.Column.ID, id)
+                }
+            }.decodeSingle()
 
     override suspend fun deleteCourseWork(id: String): CourseWorkDto =
         postgrest[Supabase.Table.COURSE_WORKS]
--- app/src/main/java/app/edumate/domain/repository/CourseWorkRepository.kt
+++ app/src/main/java/app/edumate/domain/repository/CourseWorkRepository.kt
@@ -1,6 +1,7 @@
 package app.edumate.domain.repository
 
 import app.edumate.data.remote.dto.courseWork.CourseWorkDto
+import app.edumate.data.remote.dto.material.MaterialDto
 
 interface CourseWorkRepository {
     suspend fun createCourseWork(courseWork: CourseWorkDto): CourseWorkDto
@@ -8,6 +9,16 @@ interface CourseWorkRepository {
     suspend fun getCourseWorks(courseId: String): List<CourseWorkDto>
 
     suspend fun getCourseWork(id: String): CourseWorkDto?
+
+    suspend fun updateCourseWork(
+        id: String,
+        title: String,
+        description: String?,
+        choices: List<String>?,
+        materials: List<MaterialDto>?,
+        maxPoints: Int?,
+        dueTime: String?,
+    ): CourseWorkDto
 
     suspend fun deleteCourseWork(id: String): CourseWorkDto
 }
