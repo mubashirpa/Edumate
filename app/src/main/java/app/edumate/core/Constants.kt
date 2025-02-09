d76db8eefb2f8c4b5049eb18084334da9895c91f
Mubashir P A <mubashirpa2002@gmail.com>
2025-02-09 17:22:32 +0530

    refactor: Update student submission constant name
    This commit updates the constant name for retrieving student submissions from `get_or_create_student_submission` to `get_or_insert_student_submission`.

--- app/src/main/java/app/edumate/core/Constants.kt
+++ app/src/main/java/app/edumate/core/Constants.kt
@@ -67,7 +67,7 @@ object Supabase {
         const val TURN_IN_STUDENT_SUBMISSION = "turn_in_student_submission"
         const val INSERT_ANNOUNCEMENT_COMMENT = "insert_announcement_comment"
         const val INSERT_MEMBER = "insert_member"
-        const val GET_STUDENT_SUBMISSION = "get_or_create_student_submission"
+        const val GET_STUDENT_SUBMISSION = "get_or_insert_student_submission"
         const val GET_STUDENT_SUBMISSIONS_LIST = "get_student_submissions_list"
     }
 
