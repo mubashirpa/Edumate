8b6f44e7aeca3d2762f0eae792c9b30d0c488bda
Mubashir P A <mubashirpa2002@gmail.com>
2025-02-10 12:09:00 +0530

    feat: Adjust Scaffold content window insets for edge-to-edge display
    This commit updates the `contentWindowInsets` of the `Scaffold` in the `CourseDetailsScreen` to include the display cutout area, ensuring content is displayed correctly in edge-to-edge scenarios.

--- app/src/main/java/app/edumate/presentation/courseDetails/CourseDetailsScreen.kt
+++ app/src/main/java/app/edumate/presentation/courseDetails/CourseDetailsScreen.kt
@@ -3,11 +3,13 @@ package app.edumate.presentation.courseDetails
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.displayCutout
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.union
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -91,7 +93,10 @@ fun CourseDetailsContent(
                 courseId = courseWithMembers.id!!,
             )
         },
-        contentWindowInsets = WindowInsets.systemBars.only(WindowInsetsSides.Bottom),
+        contentWindowInsets =
+            WindowInsets.systemBars
+                .union(WindowInsets.displayCutout)
+                .only(WindowInsetsSides.Bottom),
     ) { innerPadding ->
         CourseDetailsNavHost(
             navController = navController,
