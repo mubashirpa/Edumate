17c7eb147e8da867789140b856e15b4b2470a17f
Mubashir P A <mubashirpa2002@gmail.com>
2025-02-12 19:13:35 +0530

    feat: Display student count in teaching list item
    This commit adds the ability to display the total number of students in the teaching list item.

--- app/src/main/java/app/edumate/presentation/teaching/components/TeachingListItem.kt
+++ app/src/main/java/app/edumate/presentation/teaching/components/TeachingListItem.kt
@@ -11,13 +11,13 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.People
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.InputChip
-import androidx.compose.material3.InputChipDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,6 +27,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,7 +35,6 @@ import androidx.compose.ui.unit.dp
 import app.edumate.R
 import app.edumate.domain.model.course.Course
 import app.edumate.domain.model.user.User
-import app.edumate.presentation.components.UserAvatar
 import app.edumate.presentation.theme.EdumateTheme
 
 @Composable
@@ -92,17 +92,21 @@ fun TeachingListItem(
                 selected = false,
                 onClick = {},
                 label = {
-                    Text(text = teachingCourse.owner?.name.orEmpty())
+                    val totalStudents = teachingCourse.totalStudents ?: 0
+                    Text(
+                        text =
+                            pluralStringResource(
+                                R.plurals.students,
+                                totalStudents,
+                                totalStudents,
+                            ),
+                    )
                 },
                 modifier = Modifier.align(Alignment.End),
-                avatar = {
-                    UserAvatar(
-                        id = teachingCourse.owner?.id.orEmpty(),
-                        fullName = teachingCourse.owner?.name.orEmpty(),
-                        photoUrl = teachingCourse.owner?.photoUrl,
-                        size = InputChipDefaults.AvatarSize,
-                        shape = MaterialTheme.shapes.medium,
-                        textStyle = MaterialTheme.typography.labelSmall,
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.People,
+                        contentDescription = null,
                     )
                 },
             )
