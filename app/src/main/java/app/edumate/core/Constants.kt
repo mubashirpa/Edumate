002fe5a8ae43d9c94f680c260a25b9e2f77e1e7b
Mubashir P A <mubashirpa2002@gmail.com>
2025-02-28 21:28:12 +0530

    feat: Update backdrop image URL

--- app/src/main/java/app/edumate/core/Constants.kt
+++ app/src/main/java/app/edumate/core/Constants.kt
@@ -4,7 +4,7 @@ import androidx.datastore.preferences.core.stringPreferencesKey
 
 object Constants {
     const val BACKDROP_GET_STARTED =
-        "https://firebasestorage.googleapis.com/v0/b/edu-mate-app.appspot.com/o/get_started.jpg?alt=media&token=6b9e6215-c0a4-4046-a15d-42cb5a102986"
+        "https://res.cloudinary.com/dgorry0hf/image/upload/f_auto,q_auto/edumate-get-started"
     const val BACKDROP_GET_STARTED_LOCAL = "file:///android_asset/images/get_started.png"
     const val EDUMATE_BASE_URL = "https://edumate-learning.web.app/"
     const val WEB_GOOGLE_CLIENT_ID =
