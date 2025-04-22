60c768299c33e3a49a3d321137b5805f2e4f8939
Mubashir P A <mubashirpa2002@gmail.com>
2025-04-22 14:38:49 +0530

    feat: Update versionCode and versionName
    This commit updates the `versionCode` and `versionName` in the app's `build.gradle.kts` file.
    - Sets `versionCode` to 10100 (MAJOR * 10000 + MINOR * 100 + PATCH).
    - Sets `versionName` to "1.1.0".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "app.edumate"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 10100 // Format: MAJOR * 10000 + MINOR * 100 + PATCH
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
